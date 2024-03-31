53421c671d92b09f56f9d4ddd8b41064419ecba5
Kaique Luz <60513687+KaiqueLuz23@users.noreply.github.com>
2024-03-31 11:17:38 -0300

    Update api_status.tf

--- infra/api_status.tf
+++ infra/api_status.tf
@@ -20,5 +20,4 @@ resource "aws_api_gateway_integration" "lambda_integration_status" {
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.extrato_account_lambda.invoke_arn
-  # uri                     = aws_lambda_function.bank_account_lambda.invoke_arn
 }
